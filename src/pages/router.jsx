834cfde074271d6564d7fbe982e44ae8f1c73a11
alexbranco85 <alexbranco85@gmail.com>
2023-06-14 20:26:15 -0300

    Adicionando produtos no Redux , criação da single product, categoria e mais outras coisas

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Provider } from "react-redux"
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom'
 import Home from "./Home"
+import CategoryPage from "./Category"
+import SingleProduct from "./Single"
 
 const Router = () => {
   return (
@@ -14,6 +17,21 @@ const Router = () => {
           exact
           path="/"
           element={<Home />}
+        />
+        <Route
+          exact
+          path="/categoria/:id"
+          element={<CategoryPage />}
+        />
+        <Route
+          exact
+          path="/todos"
+          element={<CategoryPage />}
+        />
+        <Route
+          exact
+          path="/produto/:sku"
+          element={<SingleProduct />}
         />
       </Routes>
     </BrowserRouter>
